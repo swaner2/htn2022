321caf6c46890b8307b6db8ebf6b0bf39b764064
swaner2 <j.chen1904@gmail.com>
2022-09-17 18:57:56 -0400

    hopefully this work

--- example_extension/popup.js
+++ example_extension/popup.js
@@ -1,41 +1,7 @@
-const text = document.getElementById( 'notify-text' );
-const notify = document.getElementById( 'notify-button' );
+let button = document.getElementById('notify-button');
+button.textContent = 'I drank water!'
+document.body.insertAdjacentElement('afterbegin', button);
 
-notify.addEventListener( 'click', () => {
-  chrome.runtime.sendMessage( '', {
-    type: 'notification',
-    message: text.value
-  });
-} );
-
-
-const counter = document.getElementById( 'notify-count' );
-
-chrome.storage.local.get( ['notifyCount'], data => {
-  let value = data.notifyCount || 0;
-  counter.innerHTML = value;
-} );
-
-chrome.storage.onChanged.addListener( ( changes, namespace ) => {
-  if ( changes.notifyCount ) {
-    let value = changes.notifyCount.newValue || 0;
-    counter.innerHTML = value;
-  }
-});
-
-const notify = message => {
-  chrome.storage.local.get( ['notifyCount'], data => {
-    let value = data.notifyCount || 0;
-    chrome.storage.local.set({ 'notifyCount': Number( value ) + 1 });
-  } );
-
-  return chrome.notifications.create(
-    '',
-    {
-      type: 'basic',
-      title: 'Notify!',
-      message: message || 'Notify!',
-      iconUrl: '128.png',
-    }
-  );
-};
+button.addEventListener('click', () => {
+    console.log('Clicked');
+  })
