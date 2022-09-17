16efede5a8ed80c00e7016f206cbe1b08207ee96
dianzrong <95876281+dianzrong@users.noreply.github.com>
2022-09-17 16:05:26 -0400

    Update background.js
    For test

--- example_extension/background.js
+++ example_extension/background.js
@@ -1,5 +1,11 @@
 chrome.alarms.onAlarm.addListener((alarm) => {
     if (alarm.name === "testAlarm") {
-        //our alarm is running, send notification
+       chrome.notifications.create('test', {
+            type: 'basic',
+            iconUrl: 'images/1.png',
+            title: 'Test Message',
+            message: 'You are awesome!',
+            priority: 2
+        });
     }
 });
