53ae910c754566dd58802cf35bf0a5a6d987d302
dianzrong <95876281+dianzrong@users.noreply.github.com>
2022-09-17 13:27:09 -0400

    Update popup.js

--- example_extension/popup.js
+++ example_extension/popup.js
@@ -7,3 +7,41 @@ notify.addEventListener( 'click', () => {
     message: text.value
   });
 } );
+
+
+const reset = document.getElementById( 'notify-reset' );
+const counter = document.getElementById( 'notify-count' );
+
+chrome.storage.local.get( ['notifyCount'], data => {
+  let value = data.notifyCount || 0;
+  counter.innerHTML = value;
+} );
+
+chrome.storage.onChanged.addListener( ( changes, namespace ) => {
+  if ( changes.notifyCount ) {
+    let value = changes.notifyCount.newValue || 0;
+    counter.innerHTML = value;
+  }
+});
+
+reset.addEventListener( 'click', () => {
+  chrome.storage.local.clear();
+  text.value = '';
+} );
+
+const notify = message => {
+  chrome.storage.local.get( ['notifyCount'], data => {
+    let value = data.notifyCount || 0;
+    chrome.storage.local.set({ 'notifyCount': Number( value ) + 1 });
+  } );
+
+  return chrome.notifications.create(
+    '',
+    {
+      type: 'basic',
+      title: 'Notify!',
+      message: message || 'Notify!',
+      iconUrl: './assets/icons/128.png',
+    }
+  );
+};
