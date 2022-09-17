98ca8aa8ca03829c9f01311c98589f4b2eb97b46
swaner2 <j.chen1904@gmail.com>
2022-09-17 18:36:58 -0400

    hopefully this work

--- example_extension/popup.js
+++ example_extension/popup.js
@@ -1,41 +1,21 @@
-const text = document.getElementById( 'notify-text' );
-const notify = document.getElementById( 'notify-button' );
 
-notify.addEventListener( 'click', () => {
-  chrome.runtime.sendMessage( '', {
-    type: 'notification',
-    message: text.value
-  });
-} );
+const button = document.createElement('button');
+button.textContent = 'I drank water!'
+document.body.insertAdjacentElement('afterbegin', button);
 
+function clickButton() {
+    button.addEventListener('click', () => {
+        console.log('Clicked');
+    })
+}
 
-const counter = document.getElementById( 'notify-count' );
+chrome.notification.create({
+        type: 'basic',
+        title: 'Water Notification',
+        message: 'Time to drink water!',
+        iconUrl: '128.png',
+        buttons: [{title: 'I drank water!'}],
+        priority: 2
+    });
 
-chrome.storage.local.get( ['notifyCount'], data => {
-  let value = data.notifyCount || 0;
-  counter.innerHTML = value;
-} );
-
-chrome.storage.onChanged.addListener( ( changes, namespace ) => {
-  if ( changes.notifyCount ) {
-    let value = changes.notifyCount.newValue || 0;
-    counter.innerHTML = value;
-  }
-});
-
-const notify = message => {
-  chrome.storage.local.get( ['notifyCount'], data => {
-    let value = data.notifyCount || 0;
-    chrome.storage.local.set({ 'notifyCount': Number( value ) + 1 });
-  } );
-
-  return chrome.notifications.create(
-    '',
-    {
-      type: 'basic',
-      title: 'Notify!',
-      message: message || 'Notify!',
-      iconUrl: '128.png',
-    }
-  );
-};
+chrome.notification.onButtonClicked.addEventListener()
