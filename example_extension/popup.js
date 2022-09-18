10ee149ba6ec5be41dec94150de99ddcc580220a
dianzrong <95876281+dianzrong@users.noreply.github.com>
2022-09-18 00:56:11 -0400

    Update popup.js

--- example_extension/popup.js
+++ example_extension/popup.js
@@ -3,11 +3,11 @@ button.textContent = 'I drank water!'
 document.body.insertAdjacentElement('afterbegin', button);
 
 let clicker_counter = 0
+let progress = 0
 
 button.addEventListener('click', () => {
     console.log('Clicked');
     clicker_counter = clicker_counter + 1;
-    let progress = 0
     progress = clicker_counter * 0.05
     progress = progress.toFixed(2) * 100 + '%'
   })
