569887a7c9b9cc8b9b445cc523dd379fd5a65803
Jessica <86748529+swaner2@users.noreply.github.com>
2022-09-18 02:57:49 -0400

    Update popup.js
    added console tracker for number of clicks

--- example_extension/popup.js
+++ example_extension/popup.js
@@ -11,6 +11,7 @@ let progress = 0
 let percent = 0
 
 button.addEventListener('click', () => {
+    console.log('clicked');
     clicker_counter = clicker_counter + 1;
     progress = clicker_counter * 0.05
     progress = progress.toFixed(2) * 100 + '%'
