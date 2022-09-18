23b90d9b585b369be64bcd9761b6f0ca452fc4d8
dianzrong <95876281+dianzrong@users.noreply.github.com>
2022-09-18 00:33:36 -0400

    Update popup.js

--- example_extension/popup.js
+++ example_extension/popup.js
@@ -8,5 +8,5 @@ let progress = 0%
 button.addEventListener('click', () => {
     console.log('Clicked');
     clicker_counter = clicker_counter + 1;
-    progress = progress + 5%
-  })
+    progress = progress + 5%;
+  }
